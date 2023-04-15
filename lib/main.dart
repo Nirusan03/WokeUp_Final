b7de880b341d3aa14bb6b15586e2cad8d995d8e9
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-15 22:23:37 +0530

    creating splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white10,
+                Colors.greenAccent,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Welcome to Elder Guardian'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
